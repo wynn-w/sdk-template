baf93bee0cd107d236b88ef3da13796a3c29a603
wynn-w <wwwynn22@gmail.com>
2024-09-19 14:47:02 +0800

    refactor: 发布脚本重构

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,13 +1,36 @@
 const { exec } = require('child_process');
 
-exec('cd dist && yalc publish', (error, stdout, stderr) => {
-    if (error) {
-        console.log(`Publish Error: ${error}`);
-        return;
+async function doLocalPublish() {
+    return await handleCmd('cd dist && yalc publish')
+}
+function handleCmd(cmd) {
+    return new Promise((res, rej) => {
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`CMD: ${cmd}, execute Error: ${error}`);
+                rej(`Publish Error: ${error}`)
+                return
+            }
+            if (stderr) {
+                console.log(`CMD: ${cmd}, stderr: ${stderr}`);
+                rej(`stderr: ${stderr}`)
+                return;
+            }
+            console.log(stdout);
+            res()
+        });
+    })
+}
+async function doPackage() {
+    return await handleCmd('cd dist && npm pack')
+}
+
+(async () => {
+    const isLocal = Boolean(process.env.LOCAL_ENV)
+    if(isLocal){
+        await doLocalPublish()
+        console.log('local publish finished')
     }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(stdout);
-});
+    await doPackage()
+    console.log('pack finished')
+})()
